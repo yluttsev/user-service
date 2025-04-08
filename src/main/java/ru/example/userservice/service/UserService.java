d6dbe82a0030e4c0b41aa5d44cc077b6b47573be
Yuri Luttsev <floomyz@vk.com>
2025-04-08 18:08:32 +0300

    Исправлены метод создания User и получения по ID в UserService

--- src/main/java/ru/example/userservice/service/UserService.java
+++ src/main/java/ru/example/userservice/service/UserService.java
@@ -2,46 +2,36 @@ package ru.example.userservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.example.userservice.dto.UserDto;
+import ru.example.userservice.dto.request.CreateUserRequest;
 import ru.example.userservice.entity.User;
+import ru.example.userservice.mapper.UserMapper;
 import ru.example.userservice.repository.UserRepository;
-
-import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    @Transactional
-    public UserDto createUser(UserDto userDto) {
-        if (userRepository.existsByEmail(userDto.getEmail())) {
-            throw new IllegalArgumentException("Email already exists");
+    public UserDto create(CreateUserRequest createUserRequest) {
+        if (userRepository.existsByEmail(createUserRequest.email())) {
+            throw new IllegalArgumentException("User with email '%s' already exists".formatted(createUserRequest.email()));
         }
-
-        User user = new User();
-        user.setCategoryId(userDto.getCategoryId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setTotalSpent(userDto.getTotalSpent() != null ? userDto.getTotalSpent() : BigDecimal.ZERO);
-
-        User savedUser = userRepository.save(user);
-        return convertToDto(savedUser);
+        User user = User.builder()
+                .email(createUserRequest.email())
+                .name(createUserRequest.name())
+                .category(createUserRequest.category())
+                .build();
+        user = userRepository.save(user);
+        return userMapper.mapEntityToDto(user);
     }
 
-    public UserDto getUserById(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-        return convertToDto(user);
-    }
-
-    private UserDto convertToDto(User user) {
-        UserDto dto = new UserDto();
-        dto.setId(user.getId());
-        dto.setName(user.getName());
-        dto.setEmail(user.getEmail());
-        dto.setTotalSpent(user.getTotalSpent());
-        return dto;
+    public UserDto getById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("User with id '%d' not found")
+        );
+        return userMapper.mapEntityToDto(user);
     }
 }
