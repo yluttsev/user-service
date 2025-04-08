4613071cad3785f9cc0e989b0b8d1d79eab97b1e
exitae337 <exitae337@gmail.com>
2025-04-08 14:07:24 +0300

    Добавление ProductRepository

--- src/main/java/ru/example/userservice/entity/Product.java
+++ src/main/java/ru/example/userservice/entity/Product.java
@@ -3,6 +3,9 @@ package ru.example.userservice.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
@@ -23,5 +26,21 @@ public class Product {
 
     @Column(nullable = false)
     private Long category;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Product that = (Product) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 
 }
