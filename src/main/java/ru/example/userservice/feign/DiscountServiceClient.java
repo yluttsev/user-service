9413087d93217114394b107756e8dad3f9cfc68c
exitae337 <exitae337@gmail.com>
2025-04-08 13:41:07 +0300

    Добавление Feign клиента: DiscountServiceClient

---
+++ src/main/java/ru/example/userservice/feign/DiscountServiceClient.java
@@ -0,0 +1,24 @@
+package ru.example.userservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "discount-service", url = "${discount.service.url}")
+public interface DiscountServiceClient {
+
+    @GetMapping("/discount/fixed")
+    BigDecimal getFixedDiscount(
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("product_category") long productCategoryId,
+            @RequestParam("client_category") long clientCategoryId);
+
+    @GetMapping("/discount/variable")
+    BigDecimal getVariableDiscount(
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("product_category") long productCategoryId,
+            @RequestParam("client_category") long clientCategoryId);
+
+}
